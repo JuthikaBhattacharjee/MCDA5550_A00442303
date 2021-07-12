e3f6fffcf575451889475d94be2cffd896f396a6
Juthika Bhattacharjee <Juthika.Bhattacharjee@smu.ca>
2021-07-11 23:39:00 -0300

    Added the Hotel Management Code

---
+++ inClassHotelReservation/src/main/java/com/example/demo/InClassHotelReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InClassHotelReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InClassHotelReservationApplication.class, args);
+	}
+
+}
